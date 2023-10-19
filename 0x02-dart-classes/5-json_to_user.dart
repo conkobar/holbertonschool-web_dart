ce25ec640044bb55094c0c2f69aba91906c68aa7
conkobar <connorkleine@gmail.com>
2023-10-18 22:33:04 -0500

    task 5 fixed object

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -22,10 +22,10 @@ class User {
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson['id'],
       name: userJson['name'],
       age: userJson['age'],
       height: userJson['height'],
+      id: userJson['id'],
     );
   }
 
