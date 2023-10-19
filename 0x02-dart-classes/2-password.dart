1383d139841e4c868c3fbfae08230595b10063bd
conkobar <connorkleine@gmail.com>
2023-10-18 22:07:33 -0500

    task 0

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,32 @@
+class Password {
+  String password = '';
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+
+  bool isValid() {
+    return password.length >= 8 &&
+        password.length <= 16 &&
+        password.contains(RegExp(r'[A-Z]')) &&
+        password.contains(RegExp(r'[a-z]')) &&
+        password.contains(RegExp(r'[0-9]'));
+  }
+}
+
+void main() {
+  final ps = Password();
+  ps.password = "Just3z";
+  if (ps.isValid() == true)
+    print("${ps.password} is a Valid Password");
+  else
+    print("${ps.password} is Not a Valid Password");
+  print("${ps.toString()}");
+  ps.password = "ShouldWorkf7ne";
+  if (ps.isValid() == true)
+    print("${ps.password} is a Valid Password");
+  else
+    print("${ps.password} is Not a Valid Password");
+  print("${ps.toString()}");
+}
